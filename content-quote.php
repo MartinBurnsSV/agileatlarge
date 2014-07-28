ab405cd9cc3db114708566205e308c59be9efb74
MartinBurns <martinburnssv@gmail.com>
2014-07-28 13:27:37 +0200

    Child Theme for AgileAtLarge

--- content-quote.php
+++ content-quote.php
@@ -6,14 +6,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="top-info"<?php serene_blockquote_color(); ?>>
+	<div class="top-info"<?php echo serene_get_post_background(); ?>>
 		<div class="post-content clearfix">
 			<div class="entry-content clearfix">
 				<?php echo serene_get_blockquote_in_content(); ?>
+		<?php if ( ! is_single() ) : ?>
+				<p><cite><?php echo the_title();  ?></cite></p>
+		<?php endif; ?>
 			</div>
 		</div> <!-- .post-content -->
 
-		<?php serene_post_meta_info(); ?>
+
 
 		<?php if ( ! is_single() ) : ?>
 		<a href="<?php the_permalink(); ?>" class="et-quote-more-link"><?php esc_html_e( 'Read More', 'Serene' ); ?></a>
@@ -22,16 +25,19 @@
 
 	<?php if ( is_single() ) : ?>
 
-	<div class="bottom-info"<?php echo serene_get_post_background(); ?>>
-		<div class="post-content">
-			<h1 class="title"><?php the_title(); ?></h1>
+	<div class="bottom-info">
+		<div class="post-content"<?php serene_blockquote_color(); ?>>
+			
 
-			<?php et_postinfo_meta(); ?>
+
 
 			<div class="entry-content clearfix">
 			<?php
 				the_content();
+			?>
+				<p><cite><?php echo the_title();  ?></cite></p>
 
+			<?php
 				wp_link_pages( array(
 					'before'         => '<p><strong>' . esc_attr__( 'Pages', 'Serene' ) . ':</strong> ',
 					'after'          => '</p>',
